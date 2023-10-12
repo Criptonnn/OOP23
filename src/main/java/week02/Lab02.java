fdef06f6ecbdfd11a4e2a6420e81c846cea86222
Criptonnn <eemaemir1@gmail.com>
2023-10-12 19:31:43 +0200

    finished the week02 lab tasks from 3-10

--- src/main/java/week02/Lab02.java
+++ src/main/java/week02/Lab02.java
@@ -1,5 +1,6 @@
 package week02;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lab02 {
@@ -8,6 +9,15 @@ public class Lab02 {
         //password();
         //sumThree();
         //sumNumbers();
+        //printNumbersBetween();
+        //powerOfTwo();
+        //printText();
+        //printStars(5);
+        //drawPyramid(5);
+        //drawPyramidReversed(5);
+        //drawNumbersPyramid(5);
+        //drawNumbersPyramidV2(5);
+        //guessingGame();
 
     }
 
@@ -57,6 +67,96 @@ public class Lab02 {
         System.out.println("Sum in the end: " + sum);
     }
 
+    public static void printNumbersBetween() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int x = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int y = scanner.nextInt();
 
+        while (x <= y) {
+            System.out.println(x);
+            x++;
+        }
+    }
 
+    public static void powerOfTwo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter exponent: ");
+        int x = scanner.nextInt();
+        int sum = 0;
+        for(int i = 0; i <= x; i++) {
+            sum += Math.round((Math.pow(2, i)));
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+
+    public static void printStars(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public static void drawPyramid(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void drawPyramidReversed(int n) {
+        for(int i = n; i >= 0; i--) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void drawNumbersPyramid(int n) {
+        for(int i = 1; i <= n + 1; i++) {
+            for(int j = 1; j < i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void drawNumbersPyramidV2(int n) {
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void guessingGame() {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randomNum = random.nextInt(100) + 1;
+        int guesses = 0;
+        while (true) {
+            System.out.print("Guess: ");
+            int guess = scanner.nextInt();
+            guesses++;
+            if(guess == randomNum) {
+                System.out.println("Congratulations!");
+                break;
+            }
+            if(guess > randomNum) {
+                System.out.println("Your number is greater, number of guesses: " + guesses);
+            }
+            if(guess < randomNum) {
+                System.out.println("Your number is smaller, number of guesses: " + guesses);
+            }
+        }
+    }
 }
+
