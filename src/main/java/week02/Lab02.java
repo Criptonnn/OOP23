d385126d2dacae0437c2583cd226917eff77251b
Criptonnn <eemaemir1@gmail.com>
2023-10-12 17:06:33 +0200

    prva tri zadatka week02 uradjena

--- src/main/java/week02/Lab02.java
+++ src/main/java/week02/Lab02.java
@@ -1,13 +1,62 @@
 package week02;
 
+import java.util.Scanner;
+
 public class Lab02 {
 
     public static void main(String[] args) {
+        //password();
+        //sumThree();
+        //sumNumbers();
 
     }
 
-    public void helloBro() {
-        System.out.println("Sup G");
+    public static void password() {
+        Scanner scanner = new Scanner(System.in);
+        String pwd;
+        while(true) {
+            System.out.print("Guess: ");
+            pwd = scanner.nextLine();
+            if(pwd.equals("carrot")) {
+                System.out.println("Bravo majstore!");
+                break;
+            }
+            else {
+                System.out.println("Wrong!");
+            }
+        }
     }
+
+    public static void sumThree() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int num;
+        int i = 0;
+        while (i < 3) {
+            System.out.print("Input number: ");
+            System.out.print("Enter new number: ");
+            num = scanner.nextInt();
+            sum += num;
+            i++;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void sumNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            System.out.print("Input number: ");
+            int num = scanner.nextInt();
+            if(num == 0) {
+                break;
+            }
+            sum += num;
+            System.out.println("Sum now: " + sum);
+        }
+        System.out.println("Sum in the end: " + sum);
+    }
+
+
 
 }
